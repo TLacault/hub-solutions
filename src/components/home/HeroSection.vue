699daa0454a19444480e27f18c8fa1172377d2fa
Tim Lacault <contact@timlacault.dev>
2025-07-30 15:32:56 +0200

    update: break down home view into components

---
+++ src/components/home/HeroSection.vue
@@ -0,0 +1,173 @@
+<template>
+  <section id="hero">
+    <div class="shape-01 anim_appear-shape">
+      <img src="@/assets/img/home/shape_01.png" alt="Hero Image" />
+    </div>
+
+    <h1 class="anim_appear">HUB Solutions</h1>
+    <h2 class="anim_appear">
+      Formation et conseil en prévention des risques professionnels
+    </h2>
+    <div class="links anim_appear">
+      <router-link to="/formations">
+        <p>Formations</p>
+        <i class="ri-arrow-right-circle-line"></i>
+      </router-link>
+      <router-link to="/audits">
+        <p>Audits</p>
+        <i class="ri-arrow-right-circle-line"></i>
+      </router-link>
+    </div>
+
+    <div class="scroll-down anim_appear">
+      <p>Découvrir</p>
+      <i class="ri-arrow-down-double-fill bounce"></i>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.shape-01 {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: -1;
+  top: 0;
+  height: 100dvh;
+  animation-delay: 0.5s;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+#hero {
+  h1 {
+    font-family: "Arial Black", sans-serif;
+    font-size: clamp(2rem, 10vw, 5rem);
+    font-weight: bold;
+    color: var(--text);
+    text-align: center;
+    margin-bottom: 1rem;
+
+    background-clip: text;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(
+      90deg,
+      var(--logo_3),
+      var(--logo_5),
+      var(--logo_6),
+      var(--logo_1),
+      var(--logo_2)
+    );
+    color: transparent;
+    animation-delay: 1s;
+  }
+
+  h2 {
+    font-size: clamp(1.5rem, 5vw, 2rem);
+    font-weight: 600;
+    color: var(--text-700);
+    text-align: center;
+    margin-bottom: 2rem;
+    width: 90%;
+    max-width: 500px;
+    text-wrap: wrap;
+    animation-delay: 1.2s;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2rem;
+    animation-delay: 1.6s;
+
+    & a {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+      padding: 1rem 2rem;
+      border-radius: 100px;
+      /* width: fit-content; */
+      background: linear-gradient(45deg, #0f67ff, #c5dafc 80%);
+      color: var(--text);
+      cursor: pointer;
+
+      & p {
+        font-size: clamp(1rem, 2vw, 1.2rem);
+        font-weight: 600;
+        margin: 0;
+        color: var(--background);
+        text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      }
+
+      & i {
+        font-size: clamp(1.5rem, 2vw, 1.5rem);
+        font-weight: 600;
+        color: var(--background);
+        text-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      }
+
+      &:hover {
+        transform: scale(1.05);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      }
+    }
+  }
+}
+
+@media (max-width: 500px) {
+  .links {
+    flex-direction: column !important;
+    align-items: center;
+    justify-content: center;
+
+    & a {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+    }
+  }
+}
+
+.scroll-down {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 2rem;
+  color: var(--text);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+
+  animation-delay: 2.4s;
+
+  & p {
+    font-size: 1rem;
+    font-weight: 600;
+    color: var(--text);
+    text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    opacity: 0.8;
+  }
+
+  & i {
+    border-radius: 100px;
+    padding: 0.5rem 0;
+    animation: bounce 2s infinite;
+    color: var(--background);
+    text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    background: linear-gradient(45deg, var(--primary), var(--secondary));
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/home/ValeursSection.vue
@@ -0,0 +1,120 @@
+<template>
+  <section id="valeurs">
+    <div class="container">
+      <div class="shape-02">
+        <img src="@/assets/img/home/shape_02.png" alt="Valeur Background" />
+      </div>
+      <div class="title gradient_text">Nos valeurs</div>
+      <div class="cards">
+        <div class="card">
+          <i class="ri-verified-badge-fill"></i>
+          <div class="name">Confiance</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua !
+          </p>
+        </div>
+        <div class="card">
+          <i class="ri-medal-fill"></i>
+          <div class="name">Professionnalisme</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua !
+          </p>
+        </div>
+        <div class="card">
+          <i class="ri-shake-hands-fill"></i>
+          <div class="name">Diversité</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua !
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#valeurs {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 50vh;
+  gap: 2rem;
+  margin-top: 50vh;
+  margin-bottom: 30vh;
+
+  & .title {
+    /* font-size: 4rem; */
+    font-size: clamp(3rem, 5vw, 4rem);
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+}
+
+.shape-02 {
+  position: absolute;
+  z-index: -1;
+
+  & img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    opacity: 0.1;
+  }
+}
+
+#valeurs .cards {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  width: 100%;
+
+  & .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 350px;
+    max-width: 80vw;
+    padding: 2rem;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+
+    &:hover {
+      transform: translateY(-5px);
+      scale: 1.02;
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+    }
+
+    i {
+      font-size: 2.5rem;
+      color: var(--primary);
+    }
+
+    .name {
+      font-size: 1.4rem;
+      font-weight: bold;
+      color: var(--text);
+      text-align: center;
+    }
+
+    p {
+      font-size: 1rem;
+      color: var(--text-700);
+      line-height: 1.5;
+      opacity: 0.8;
+    }
+  }
+}
+</style>
